ca7dbb6d924fb001969e2a73d211871304e3ccb1
Brienyll <brienyll29@gmail.com>
2019-01-26 07:54:08 -0800

    added lazy loading for todos component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { TodosListComponent } from './todos/todos-list/todos-list.component';
 
 const routes: Routes = [
-  {
-    path: '', component: HomeComponent
+  { path: '',
+    component: HomeComponent
   },
-  {
-    path: 'about', component: AboutComponent
+  { path: 'about',
+    component: AboutComponent
+  },
+  { path: 'todos',
+    loadChildren: './todos/todos.module#TodosModule'
   }
 ];
 
